70d821c9aaf512bdfb0c58f883808484ac3754c8
Will Boshes <will.boshes@kellogg.northwestern.edu>
2023-06-06 04:40:20 +0000

    BCrypted password in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user["last_name"] = params["user"]["last_name"]
     @user["userid"] = params["user"]["userid"]
     @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
     redirect_to "/users/#{@user["id"]}"
   end
