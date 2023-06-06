e00441fe78fdae71d77faed6bcdde1cb1ee301b2
Will Boshes <will.boshes@kellogg.northwestern.edu>
2023-06-06 05:03:36 +0000

    session conrtoller updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "username" => params["username"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "You are now logged-in."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Sorry, invalid username or password."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Sorry, invalid username or password."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 end
-  
