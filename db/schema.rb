4e875d3fddbd1913d9c98b1bc98e9e5e75e16e93
Will Boshes <will.boshes@kellogg.northwestern.edu>
2023-06-06 04:37:04 +0000

    Password masked and first and last names -> users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_09_020701) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_043457) do
   create_table "places", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_09_020701) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
