0afd2551408ff785e8b3a02c65a21fe250202ea2
Will Boshes <will.boshes@kellogg.northwestern.edu>
2023-06-07 04:54:58 +0000

    Many edits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
 
+  def index 
+    @posts = Post.all.order("created_at desc")
+  end
+  
   def new
     @post = Post.new
-    @post.place_id = params["place_id"]
   end
 
   def create
